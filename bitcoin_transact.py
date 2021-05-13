b5941fada42f42acd4983fc684e89724e6890d08
mt_9 <hyunsichang@hanyang.ac.kr>
2021-05-13 11:54:42 +0900

    making user dataframe...

--- bitcoin_transact.py
+++ bitcoin_transact.py
@@ -57,8 +57,35 @@ def show_transaction():
     plt.bar(x, y, width=0.01)
     plt.show()
 
+
+def save_user_data():
+    import pandas as pd
+    import json
+
+    df = pd.read_csv("address.csv")
+
+    for i in range(1, 47):
+        file_name = "D:\\download\coin\coin{}.json".format(i)
+        with open(file_name, 'r') as file_data:
+            line = file_data.readline()
+            while line:
+                data = json.loads(line)
+                for tx in data['tx']:
+                    for vout in tx['vout']:
+                        if vout['scriptPubKey']['type'] not in ('nulldata', 'nonstandard'):
+                            address_row = df.loc[df['address'].values == vout['scriptPubKey']['addresses']]
+                            address_row['transaction'] += 1
+                            address_row['value'] += vout['value']
+                            df.loc[df['address'].values == vout['scriptPubKey']['addresses']] = address_row
+                print("line clear")
+                line = file_data.readline()
+
+            file_data.close()
+            print("coin{} Clear.".format(i))
+
 # 9222178 transactions
 # 10541781 addresses
 # 유저 산점도 (코인 수, 거래 수를 축으로)
 # medianTime별 거래 수 막대그래프
 # abusing address 표시
+# 1L5KGyo42cd1ecyuQ3eLtvBVJDYDrTvJen
