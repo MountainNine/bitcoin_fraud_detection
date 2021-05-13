087c50109ab1bdd0218be39c848bd0dc2f9f99b3
mt_9 <hyunsichang@hanyang.ac.kr>
2021-05-13 10:28:06 +0900

    arrange code

--- bitcoin_transact.py
+++ bitcoin_transact.py
@@ -1,65 +1,61 @@
 import pandas as pd
 import json
-
-sum_nTx = 0
-address = set()
-
-for i in range(1, 47):
-    file_name = "D:\\download\coin\coin{}.json".format(i)
-    with open(file_name, 'r') as file_data:
-        line = file_data.readline()
-        while line:
-            data = json.loads(line)
-            for tx in data['tx']:
-                for vout in tx['vout']:
-                    if vout['scriptPubKey']['type'] not in ('nulldata', 'nonstandard'):
-                        address.update(vout['scriptPubKey']['addresses'])
-            line = file_data.readline()
-
-        file_data.close()
-        print("coin{} Clear.".format(i))
-
-text_file = open('address.txt', 'w')
-for a in address:
-    text_file.write(a + "\n")
-text_file.close()
-
-# ========================================================================================
-
-import json
-import pandas as pd
-
-median_list = []
-nTx = []
-
-for i in range(1, 47):
-    file_name = "D:\\download\coin\coin{}.json".format(i)
-    with open(file_name, 'r') as file_data:
-        line = file_data.readline()
-        while line:
-            data = json.loads(line)
-            median_list.append(data['mediantime'])
-            nTx.append(data['nTx'])
-            line = file_data.readline()
-
-        file_data.close()
-        print("coin{} Clear".format(i))
-
-df_transaction = pd.DataFrame({'medianTime': median_list, 'nTx': nTx})
-df_transaction.to_csv('./transaction.csv', sep=',', na_rep='NaN')
-
-# ====================================================================
-
-import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
 
-data = pd.read_csv("transaction.csv")
-x = data.get('medianTime')
-x = pd.to_datetime(x,unit='s')
-y = data.get('nTx')
-plt.bar(x, y, width=0.01)
-plt.show()
+
+def get_address():
+    sum_nTx = 0
+    address = set()
+
+    for i in range(1, 47):
+        file_name = "D:\\download\coin\coin{}.json".format(i)
+        with open(file_name, 'r') as file_data:
+            line = file_data.readline()
+            while line:
+                data = json.loads(line)
+                for tx in data['tx']:
+                    for vout in tx['vout']:
+                        if vout['scriptPubKey']['type'] not in ('nulldata', 'nonstandard'):
+                            address.update(vout['scriptPubKey']['addresses'])
+                line = file_data.readline()
+
+            file_data.close()
+            print("coin{} Clear.".format(i))
+
+    text_file = open('address.txt', 'w')
+    for a in address:
+        text_file.write(a + "\n")
+    text_file.close()
+
+
+def save_transaction():
+    median_list = []
+    nTx = []
+
+    for i in range(1, 47):
+        file_name = "D:\\download\coin\coin{}.json".format(i)
+        with open(file_name, 'r') as file_data:
+            line = file_data.readline()
+            while line:
+                data = json.loads(line)
+                median_list.append(data['mediantime'])
+                nTx.append(data['nTx'])
+                line = file_data.readline()
+
+            file_data.close()
+            print("coin{} Clear".format(i))
+
+    df_transaction = pd.DataFrame({'medianTime': median_list, 'nTx': nTx})
+    df_transaction.to_csv('./transaction.csv', sep=',', na_rep='NaN')
+
+
+def show_transaction():
+    data = pd.read_csv("transaction.csv")
+    x = data.get('medianTime')
+    x = pd.to_datetime(x, unit='s')
+    y = data.get('nTx')
+    plt.bar(x, y, width=0.01)
+    plt.show()
 
 # 9222178 transactions
 # 10541781 addresses
